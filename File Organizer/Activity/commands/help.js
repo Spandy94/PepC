96ca9c06a67baffc5868193f24ee089c883a4231
Spandy94 <pupul.spandan94@gmail.com>
2021-08-25 21:36:52 +0530

    tree

--- File Organizer/Activity/commands/help.js
+++ File Organizer/Activity/commands/help.js
@@ -1,2 +1,10 @@
-let fs=require('fs');
-let path=require("path");
+function helpFunction(){
+    console.log(`List of all commands
+    1. node main.js tree <pathName>
+    2. node main.js organize <pathName>
+    3. node main.js help`);
+}
+
+module.exports={
+    helpFn : helpFunction
+};
